9f19a74508b394bbdabfc9d28c6104cd8f2eb402
DrJSimpson <gbenedetti.1990@gmail.com>
2016-09-02 18:37:26 -0300

    joystart
    asdf

---
+++ Joysticks.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Joysticks {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		
+	}
+}
+
+class joyPair {
+	int j1;
+	int j2;
+	
+	joyPair(int j1, int j2) {
+		this.j1 = j1;
+		this.j2 = j2;
+	}
+	
+	void connect(String s) {
+		if (s.equals("j1")) {
+			
+		} else {
+			
+		}
+	}
+}
